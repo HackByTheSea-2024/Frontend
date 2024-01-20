582be712a54108e3da3a9824b63d913f1c0a19dc
mia-926 <75146430+mia-926@users.noreply.github.com>
2024-01-19 19:58:35 -0800

    addd home page

---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import HomeP from '../../Pages/HomeP';
+import vitalSyncLogo from "../../images/vitalSyncLogo.png";
+import "../../components/components.css"
+
+
+
+
+export default function NavBar() {
+
+    return (
+      <div>
+
+      <Nav className="navBar floatLeft ml-10 mb-50">
+      <Nav.Link className="mr-5 d-flex align-items-center" href="/home">
+                    <img 
+                        src={vitalSyncLogo}
+                        alt="VitalSync Logo"
+                        style={{ width: '30px', height: 'auto', marginRight: '10px' }} 
+                    />
+                    <span>VitalSync</span>
+    </Nav.Link>
+        <Nav.Link  className="mr-5" href="/home ">About Us</Nav.Link>
+        <Nav.Link  className="mr-5" href="/instructors">Patient Portal</Nav.Link>
+        <Nav.Link  className="mr-5" href="/mission">Doctor Portal</Nav.Link>
+
+      </Nav>
+
+      </div>
+    );
+  }
+
